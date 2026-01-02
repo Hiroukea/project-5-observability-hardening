c2505705b133729a7e11f26a3bae09ad5a8d60af
Snowman <ankherdene714@gmail.com>
2026-01-01 19:52:00 -0500

    Update 00-setup.ps1

--- scripts/00-setup.ps1
+++ scripts/00-setup.ps1
@@ -1,9 +1,9 @@
 # 00-setup.ps1
-# Sets region and confirms AWS CLI identity
+# Sets the region and confirms AWS CLI identity
 
 $ErrorActionPreference = "Stop"
 
-# Change if needed
+
 if (-not $env:AWS_REGION) { $env:AWS_REGION = "us-east-1" }
 
 Write-Host "Region:" $env:AWS_REGION
